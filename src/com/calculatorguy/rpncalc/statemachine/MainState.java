f24b7a3ee86def3d971c5c315ff23cf1e2a5f654
Robert Diamond <rmd6502@gmail.com>
2011-06-12 15:06:35 -0400

    Remove type from State machine

--- src/com/calculatorguy/rpncalc/statemachine/MainState.java
+++ src/com/calculatorguy/rpncalc/statemachine/MainState.java
@@ -1,10 +1,12 @@
 package com.calculatorguy.rpncalc.statemachine;
 
 
-public class MainState implements State<?> {
+public class MainState implements State {
 
-	public State handleEvent(CalcEvent<KeyValue> c) {
-		switch(c.getValue()) {
+	public State handleEvent(Object c) {
+		@SuppressWarnings("unchecked")
+		CalcEvent<KeyValue> cc = (CalcEvent<KeyValue>) c;
+		switch(cc.getValue()) {
 		case KEY_0:
 			break;
 		}
--- src/com/calculatorguy/rpncalc/statemachine/State.java
+++ src/com/calculatorguy/rpncalc/statemachine/State.java
@@ -1,5 +1,5 @@
 package com.calculatorguy.rpncalc.statemachine;
 
-public interface State<T> {
-	public State<?> handleEvent(CalcEvent<T> c);
+public interface State {
+	public State handleEvent(Object c);
 }
